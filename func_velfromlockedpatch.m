c2be11e22535f75c4eb4cd47b86070ee290ba85a
Rishav Mallick <mallickrishg@gmail.com>
2024-11-09 19:00:33 -0800

    make inverse problem estimate transition width - pseudo causality

--- func_velfromlockedpatch.m
+++ func_velfromlockedpatch.m
@@ -7,33 +7,34 @@ function dpred = func_velfromlockedpatch(m,params)
 % model parameters for inversion:
 % m(1) - updip locking limit in [km]
 % m(2) - downdip locking limit in [km]
-% m(3) - down-dip free-slip extent in [km]
+% m(3) - transition width (from lock to creep at plate rate) [km]
 % m(4) - Vplate in [mm/yr]
-% m(5) - fault dip
+% m(5) - fault dip in degrees
 % m(6) - plate thickness in [km]
 % AUTHOR:
 % Rishav Mallick, JPL, 2024
 
 % construct fault parameter vector for meshing
 % [Vpl,x0,z0,W,δ,dw]
-fparameters = [1,0,1,300e3,m(5),5e3;...
+meshsize = 2e3;
+fparameters = [1,0,1,300e3,m(5),meshsize;...
             -sind(m(5)),0,1,m(6)*1e3,m(5)+90,m(6)*1e3];
 Vplate = m(4); % plate velocity in [mm/yr]
 % station locations
 ox = params.ox;
-
+% create fault mesh
 rcv = geometry.receiver(fparameters,geometry.LDhs(params.mu,params.nu));
 
 % find patches on the megathrust (not the hinge) 
 index = rcv.Vpl>0;
 % compute distance along the megathrust in [km]
-dipdist = sqrt(rcv.xc(:,1).^2 + rcv.xc(:,2).^2)/-1e3;
+dipdist = sqrt(rcv.xc(:,1).^2 + rcv.xc(:,2).^2)./1e3;
 
 % locked patches on the fault
 Ilockedpatch = (dipdist>m(1) & dipdist<m(2) & index);
 
 % enforce plate rate below some depth
-Icreep = (dipdist > m(3) | ~index);
+Icreep = (dipdist > (m(2)+m(3)) | ~index);
 
 % imposed BCs on locked & free-creep patches
 IimposedBC = Ilockedpatch|Icreep;
@@ -61,5 +62,6 @@ sliprate(Icreep)=Vplate.*rcv.Vpl(Icreep);
 
 % return predicted velocity field 
 dpred = bigG*(sliprate-rcv.Vpl.*Vplate);
-
+% for horizontals we need to account for reference frame and block motions
+dpred(params.flag==0) = -dpred(params.flag==0) + Vplate.*(ox(params.flag==0)>0);
 end
