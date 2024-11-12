b36a2a8831e6f0f2971122b246a9792d8b6ca187
Rishav Mallick <mallickrishg@gmail.com>
2024-11-12 11:54:39 -0800

    modified code to use analytical solution and parallelize sampler

--- JOINTINVERSE_crackmodel.m
+++ JOINTINVERSE_crackmodel.m
@@ -12,11 +12,6 @@ import('geometry.*')
 % Elastic parameters (homogenous medium)
 nu = 0.25;% Poisson's ratio
 mu = 30e3;% in MPa
-faultfilename = 'megathrust2d.seg';
-
-% load megathrust mesh
-earthModel = geometry.LDhs(mu,nu);
-rcv = geometry.receiver(faultfilename,earthModel);
 
 % load data [x(km),vz(mm/yr),σz(mm/yr)]
 xpred = linspace(-50,300,500)'.*1e3;% predicted locations
@@ -41,7 +36,8 @@ W = inv(Cd);
 % m(6) - plate thickness in [km]
 
 rng(42)
-Nsamples = 1e2;
+Nsamples = 5e2;
+Nchains = 12;
 
 parameters = [];
 parameters.ox = ox;
@@ -50,30 +46,44 @@ parameters.mu = mu;
 parameters.nu = nu;
 
 % provide model initialization: ideally this is an optimization solution 
-minit = [10,100,10,20,10,100];
+minit = [100,10,16,10,100];
 
 % non-linear sampling with slice sampler
-dpred = @(m) func_velfromlockedpatch(m,parameters);
+dpred = @(m) func_velfromlockedpatch2(m,parameters);
 residuals=@(dpred,d,W) sum(diag(W))/(sum(diag(W))^2 - sum(diag(W).^2))*(dpred-d)'*W*(dpred-d);
 % setup priors (bounds)
 
-LB =  [0,50,5,15,8,90];
-UB =  [50,200,100,25,15,110];
-mnames = {'\zeta_{up-dip}';'\zeta_{down-dip}';'W';'V_{pl}';'\delta';'T_{plate}'};
+LB =  [50,5,15,8,20];
+UB =  [200,100,25,15,200];
+mnames = {'\zeta_{down-dip}';'W';'V_{pl}';'\delta';'T_{plate}'};
 
 % compute optimization solution
-mopt = lsqnonlin(@(m) sqrt(W)*(dpred(m) - d),minit,LB,UB);
-disp(mopt)
+options = optimoptions('lsqnonlin','Algorithm','interior-point',...
+    'TypicalX',[5,5,0.5,1,5],'StepTolerance',1e-3,...
+    'FiniteDifferenceStepSize',[1,1,1e-6,1e-6,1],'Display','none');
+mopt = lsqnonlin(@(m) sqrt(W)*(dpred(m) - d),minit,LB,UB,[],[],[],[],[],options);
+disp([minit' mopt'])
 
-% log-likelihood on a transformed variable (makes sampling easier)
+% log-likelihood on a logit-transformed variable (makes sampling easier)
 likelihoodfun = @(m) -0.5.*(residuals(dpred((UB-LB)./(1+exp(-m)) + LB),d,W)) - sum(m + 2*log(1+exp(-m)));
 
 tic
 disp('Begin Slicesampling')
 
-zetasamples = slicesample(mopt,Nsamples,'logpdf',likelihoodfun,'thin',2,'burnin',Nsamples/10,'width',[3,3,3,3,2,3]);
-
+% sample with slicesample and parallelize over Nchains
+zetasamples3d = zeros(Nsamples,Nchains,length(LB));
+zetasamples = zeros(Nsamples*Nchains,length(LB));
 mposterior = zeros(size(zetasamples));
+zetainit = log(mopt-LB)-log(UB-mopt);
+parfor chains = 1:Nchains
+    zetasamples3d(:,chains,:) = slicesample(zetainit,Nsamples,...
+        'logpdf',likelihoodfun,'thin',3,'burnin',Nsamples,'width',[3,1,1,1,1]);
+end
+% combine Nchains into a single matrix
+for i = 1:Nchains
+    zetasamples(Nsamples*(i-1) + 1:Nsamples*i,:) = squeeze(zetasamples3d(:,i,:));
+end
+% transform from logit coordinates to natural coordinates
 for i = 1:length(LB)
     mposterior(:,i) = (UB(i)-LB(i))./(1+exp(-zetasamples(:,i))) + LB(i);
 end
@@ -86,23 +96,30 @@ figure(1),clf
 plot_joint_post_pdf(mposterior,mnames);
 
 % plot predictions
-vxpred = zeros(length(xpred),Nsamples);
-vzpred = zeros(length(xpred),Nsamples);
-for i = 1:10:Nsamples
+Npred = 101;
+sampleid = randperm(Nchains*Nsamples,Npred);
+vxpred = zeros(length(xpred),Npred);
+vzpred = zeros(length(xpred),Npred);
+
+parfor i = 1:length(sampleid)
     params = [];
     params.ox = xpred;
     params.flag = zeros(length(xpred),1);
     params.mu = mu;
     params.nu = nu;
-    vxpred(:,i) = func_velfromlockedpatch(mposterior(i,:),params);
+    vxpred(:,i) = func_velfromlockedpatch2(mposterior(sampleid(i),:),params);
     params.flag = ones(length(xpred),1);
-    vzpred(:,i) = func_velfromlockedpatch(mposterior(i,:),params);
+    vzpred(:,i) = func_velfromlockedpatch2(mposterior(sampleid(i),:),params);
 end
 % compute predictions from optimization
+params = [];
+params.ox = xpred;
+params.mu = mu;
+params.nu = nu;
 params.flag = zeros(length(xpred),1);
-vxopt = func_velfromlockedpatch(mopt,params);
+vxopt = func_velfromlockedpatch2(mopt,params);
 params.flag = ones(length(xpred),1);
-vzopt = func_velfromlockedpatch(mopt,params);
+vzopt = func_velfromlockedpatch2(mopt,params);
 
 %% plot data predictions and fault slip rate
 figure(2),clf
@@ -110,7 +127,7 @@ set(gcf,'Color','w')
 subplot(2,1,1)
 errorbar(ox1./1e3,d1,sqrt(diag(Cd1)),'o','LineWidth',1,'CapSize',0,'MarkerFaceColor','blue'), hold on
 plot(xpred./1e3,vzpred,'-','Color',[1 0 0 0.2])
-plot(xpred./1e3,vzopt,'r-','Linewidth',2)
+plot(xpred./1e3,vzopt,'k-','Linewidth',2)
 axis tight
 % ylim([-1,1]*5)
 xlabel('x (km)'), ylabel('v_z [mm/yr]')
@@ -118,8 +135,8 @@ set(gca,'FontSize',15,'LineWidth',1.5,'TickDir','both')
 
 subplot(2,1,2)
 errorbar(ox2./1e3,d2,sqrt(diag(Cd2)),'o','LineWidth',1,'CapSize',0,'MarkerFaceColor','blue'), hold on
-plot(xpred./1e3,vxpred,'-','Color',[1 0 0 0.1])
-plot(xpred./1e3,vxopt,'r-','Linewidth',2)
+plot(xpred./1e3,vxpred,'-','Color',[1 0 0 0.2])
+plot(xpred./1e3,vxopt,'k-','Linewidth',2)
 axis tight
 % ylim([-0.2,1.2])
 xlim([-50,300])
@@ -136,32 +153,3 @@ set(gca,'FontSize',15,'LineWidth',1.5,'TickDir','both')
 % xlim([-50,300])
 % set(gca,'FontSize',15,'LineWidth',1.5,'TickDir','both')
 
-%% testing
-minit = [20,30,50,20,10,100];
-options = optimoptions('lsqnonlin','Algorithm','interior-point',...
-    'TypicalX',[5,10,5,0.5,1,5],'StepTolerance',2);
-% compute optimization solution
-mopt = lsqnonlin(@(m) sqrt(W)*(dpred(m) - d),minit,LB,UB);
-disp(mopt')
-% compute predictions from optimization
-params.flag = zeros(length(xpred),1);
-vxopt = func_velfromlockedpatch(mopt,params);
-params.flag = ones(length(xpred),1);
-vzopt = func_velfromlockedpatch(mopt,params);
-
-figure(2),clf
-set(gcf,'Color','w')
-subplot(2,1,1)
-errorbar(ox1./1e3,d1,sqrt(diag(Cd1)),'o','LineWidth',1,'CapSize',0,'MarkerFaceColor','blue'), hold on
-plot(xpred./1e3,vzopt,'r-','Linewidth',2)
-axis tight
-xlabel('x (km)'), ylabel('v_z [mm/yr]')
-set(gca,'FontSize',15,'LineWidth',1.5,'TickDir','both')
-
-subplot(2,1,2)
-errorbar(ox2./1e3,d2,sqrt(diag(Cd2)),'o','LineWidth',1,'CapSize',0,'MarkerFaceColor','blue'), hold on
-plot(xpred./1e3,vxopt,'r-','Linewidth',2)
-axis tight
-xlim([-50,300])
-xlabel('x (km)'), ylabel('v_x/v_{plate}')
-set(gca,'FontSize',15,'LineWidth',1.5,'TickDir','both')
--- func_velfromlockedpatch2.m
+++ func_velfromlockedpatch2.m
@@ -1,25 +1,24 @@
-function dpred = func_velfromlockedpatch(m,params)
+function dpred = func_velfromlockedpatch2(m,params)
 % params should have the following: 
 % Elastic parameters: mu,nu,
 % station locations: ox,
 % flags for which stations are [velx,velz]
 %
 % model parameters for inversion:
-% m(1) - updip locking limit in [km]
-% m(2) - downdip locking limit in [km]
-% m(3) - transition width (from lock to creep at plate rate) [km]
-% m(4) - Vplate in [mm/yr]
-% m(5) - fault dip in degrees
-% m(6) - plate thickness in [km]
+% m(1) - downdip locking limit in [km]
+% m(2) - transition width (from lock to creep at plate rate) [km]
+% m(3) - Vplate in [mm/yr]
+% m(4) - fault dip in degrees
+% m(5) - plate thickness in [km]
 % AUTHOR:
 % Rishav Mallick, JPL, 2024
 
 % construct fault parameter vector for meshing
 % [Vpl,x0,z0,W,δ,dw]
 meshsize = 0.5e3;
-fparameters = [1,0,1,300e3,m(5),meshsize;...
-            -sind(m(5)),0,1,m(6)*1e3,m(5)+90,m(6)*1e3];
-Vplate = m(4); % plate velocity in [mm/yr]
+fparameters = [1,0,1,300e3,m(4),meshsize;...
+            -sind(m(4)),0,1,m(5)*1e3,m(4)+90,m(5)*1e3];
+Vplate = m(3); % plate velocity in [mm/yr]
 % station locations
 ox = params.ox;
 % create fault mesh
@@ -31,38 +30,30 @@ index = rcv.Vpl>0;
 dipdist = sqrt(rcv.xc(:,1).^2 + rcv.xc(:,2).^2)./1e3;
 
 % locked patches on the fault & the hinge too
-Ilockedpatch = (dipdist>m(1) & dipdist<m(2) & index);
+Ilockedpatch = (dipdist<m(1) & index);
 
 % enforce plate rate below some depth
-Icreep = (dipdist > (m(2)+m(3)) | ~index);
+Icreep = (dipdist > (m(1)+m(2)) | ~index);
 
 % imposed BCs on locked & free-creep patches
 IimposedBC = Ilockedpatch|Icreep;
 
-% compute traction and displacement kernels
-[K,~,~,~] = geometry.computeFaultTractionKernels(rcv,rcv);
-Vplvector = rcv.Vpl.*Vplate;
-Vplvector(~index) = 0; % we don't want to account for stress from the hinge
-% long-term stressing rate
-sig0 = -K*(Vplvector);
-[Gx,Gz,~,~] = geometry.computeFaultDisplacementKernels(rcv,[ox,zeros(size(ox))]);
-
-% construct appropriate displacement kernel for all stations
-stationindex = (params.flag == 1);
-bigG = [Gz(stationindex,:);Gx(~stationindex,:)];
-
-subK = K(~IimposedBC,~IimposedBC);
-%alter the stressing rate by subtracting the stresses that develop from previously locking 
-substressrate = sig0(~IimposedBC) - K(~IimposedBC,Icreep)*(Vplvector(Icreep));
-
-subsliprate=subK\substressrate;
 sliprate=zeros(rcv.N,1);
 % using the traction free boundaries for the stress-driven creeping
 % segments while the forced slip segments are either no slip or full
 % plate-rate slip
-sliprate(~IimposedBC)=subsliprate;
+x = dipdist(~IimposedBC);
+x = (x - min(x));
+cracksliprate = Vplate.*sqrt(1 - (x./m(2)-1).^2);
+
+sliprate(~IimposedBC)=cracksliprate;
 sliprate(Icreep)=Vplate.*rcv.Vpl(Icreep);
 sliprate(~index) = 0; % the hinge is not creeping but we don't want to deal with stresses from the hinge either
+
+% construct appropriate displacement kernel for all stations
+[Gx,Gz,~,~] = geometry.computeFaultDisplacementKernels(rcv,[ox,zeros(size(ox))]);
+stationindex = (params.flag == 1);
+bigG = [Gz(stationindex,:);Gx(~stationindex,:)];
 
 % return predicted velocity field 
 dpred = bigG*(sliprate-rcv.Vpl.*Vplate);
